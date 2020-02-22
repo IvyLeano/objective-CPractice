7c0f72a2324b52697a07e2798600feade694baf7
unknown <IvyLeano@outlook.com>
2020-02-22 17:39:41 -0500

    accessor methods add

--- accessorMethods.m
+++ accessorMethods.m
@@ -3,5 +3,18 @@
 // example: for property transactionNumber, it would be:
 
 @interface Customer
+
+// declare property
 @property int transactionNumber;
+
+// setter method
+- (void) setTransactionNumber: (int) tNumber {
+    transactionNumber = tNumber;
+}
+
+// getter method
+- (int) getTransactionNumber {
+    return transactionNumber;
+}
+
 @end

4b93ab2091ca9ccefab75660e8e1c1701c180f85
unknown <IvyLeano@outlook.com>
2020-02-22 17:35:37 -0500

    interface added

---
+++ accessorMethods.m
@@ -0,0 +1,7 @@
+// default names for the getter and setter methods associated with a property are:
+// propertyName and setPropertyName
+// example: for property transactionNumber, it would be:
+
+@interface Customer
+@property int transactionNumber;
+@end
